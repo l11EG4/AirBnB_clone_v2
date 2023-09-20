65bd1488eb294e383806d41866d75ba04acfcec9
l11EG4 <zakariaberahmi@gmail.com>
2023-09-20 11:13:41 -0700

    Mega

---
+++ models/stringtemplates.py
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Made by LAIlo and MEGA
+'''ENV the str templates'''
+
+HBNB_ENV = 'HBNB_ENV'
+HBNB_MYSQL_USER = 'HBNB_MYSQL_USER'
+HBNB_MYSQL_PWD = 'HBNB_MYSQL_PWD'
+HBNB_MYSQL_HOST = 'HBNB_MYSQL_HOST'
+HBNB_MYSQL_DB = 'HBNB_MYSQL_DB'
+HBNB_TYPE_STORAGE = 'HBNB_TYPE_STORAGE'
+DB = 'db'
+FILE = 'file'
+DEV = 'dev'
+TEST = 'test'
