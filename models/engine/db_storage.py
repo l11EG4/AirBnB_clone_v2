be7e31b8a03ee0c38584de6fecd2b503535576fd
l11EG4 <zakariaberahmi@gmail.com>
2023-10-22 04:01:52 -0700

    Mega

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -6,16 +6,16 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import (create_engine)
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import Base
+from models.amenity import Amenity
 from models.state import State
 from models.city import City
 from models.user import User
 from models.place import Place
 from models.review import Review
-from models.amenity import Amenity
 
 
 class DBStorage:
-    """ Create tables in environmental"""
+    """create Tables in environment"""
     __engine = None
     __session = None
 
@@ -56,23 +56,23 @@ class DBStorage:
         return (dic)
 
     def new(self, obj):
-        """Add a new element in the table
+        """add a new element in the table
         """
         self.__session.add(obj)
 
     def save(self):
-        """Save changes
+        """save changes
         """
         self.__session.commit()
 
     def delete(self, obj=None):
-        """Delete an element in the table
+        """delete an element in the table
         """
         if obj:
             self.session.delete(obj)
 
     def reload(self):
-        """Configuration
+        """configuration
         """
         Base.metadata.create_all(self.__engine)
         sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
